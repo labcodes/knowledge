d80a5dd03ba37378fbc2f9c1635b1ce9efc5011b
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-12 09:19:38 -0300

    Create a new class based view named CreateLinkView to handle the Link form

--- links/views.py
+++ links/views.py
@@ -1,5 +1,7 @@
+from .forms import LinkForm
 from .models import Link
 from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
 from knowledge.settings import LINKS_PER_PAGE
 # Create your views here.
 
@@ -9,3 +11,13 @@ class IndexView(ListView):
     context_object_name = 'links'
     paginate_by = LINKS_PER_PAGE
     template_name = 'links/index.html'
+
+
+class CreateLinkView(FormView):
+    template_name = 'links/create-link-form.html'
+    success_url = '/'
+    form_class = LinkForm
+
+    def form_valid(self, form):
+        form.save()
+        return super(CreateLinkView, self).form_valid(form)
