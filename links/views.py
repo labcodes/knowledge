7b15e6709492336db4c68e3a41836e20c9e9fe4e
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-23 12:26:21 -0300

    Use APIView for the CreateSlackNewLinkView view

--- links/views.py
+++ links/views.py
@@ -8,6 +8,8 @@ from django.conf import settings
 from .models import Link
 from .forms import LinkForm
 
+from rest_framework.views import APIView
+
 
 class IndexView(ListView):
     model = Link
@@ -22,7 +24,7 @@ class CreateLinkView(CreateView):
     form_class = LinkForm
 
 
-class CreateSlackNewLinkView(View):
+class CreateSlackNewLinkView(APIView):
 
     def post(self, request):
         text = request.POST.get('text')
