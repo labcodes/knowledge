bd76d76a722112c65a0f02081aea390d35485e8e
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-24 11:14:53 -0300

    Include the core's urls in the urlpatterns

--- knowledge/urls.py
+++ knowledge/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('links.urls', namespace="links"))
+    url(r'^', include('core.urls', namespace="core")),
+    url(r'^links/', include('links.urls', namespace="links"))
 ]
