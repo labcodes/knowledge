566bd2468b8497843ef4ffda879b65c27c300a88
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-24 11:53:13 -0300

    Change the url to create links to create/

--- links/urls.py
+++ links/urls.py
@@ -4,6 +4,6 @@ from links import views
 
 urlpatterns = [
     url(r'^$', views.ListLinksView.as_view(), name="list-links"),
-    url(r'^create-new-link/$', views.CreateLinkView.as_view(), name="create_new_link"),
+    url(r'^create/$', views.CreateLinkView.as_view(), name="create_new_link"),
     url(r'^api/link/$', views.CreateSlackNewLinkView.as_view(), name="api-link"),
 ]
