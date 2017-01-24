30bc1a59db9cc98667a4cd0e42b3e7e89041cf86
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-24 11:18:51 -0300

    Change url of the index view

--- links/urls.py
+++ links/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from links import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^$', views.ListLinksView.as_view(), name="list-links"),
     url(r'^create-new-link/$', views.CreateLinkView.as_view(), name="create_new_link"),
     url(r'^api/link/$', views.CreateSlackNewLinkView.as_view(), name="api-link"),
 ]
