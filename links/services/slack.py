334948ac2c0d9c3465c8488dfedc7ba74e81e829
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-25 12:47:33 -0300

    Change Cintia's message to include the name of the author

--- links/services/slack.py
+++ links/services/slack.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from slackclient import SlackClient
 from knowledge.settings import SLACK_TOKEN, SLACK_CHANNEL_ID, SLACK_BOT_NAME
 
 
 def send_notification_to_slack(link):
-    text_formatted = "{}\n{}".format(link.title, link.url)
+    text_formatted = "{} enviou:\n{}\n{}".format(link.author, link.title, link.url)
 
     sc = SlackClient(SLACK_TOKEN)
     sc.api_call('chat.postMessage', channel=SLACK_CHANNEL_ID, text=text_formatted,
