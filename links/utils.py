83fac1b6f71d2f88553859426494221a9be6190f
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-25 14:44:36 -0300

    Add get_title_from_url

---
+++ links/utils.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_title_from_url(slack_url):
+    soup_url = BeautifulSoup(requests.get(slack_url).text)
+    print(soup_url)
+    title = soup_url.find('meta', property='og:title').get('content')
+
+    if not title:
+        title = soup_url.find('title').string
+
+    return title
