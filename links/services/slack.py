94d9f89cfb520f2afb97b3181b86e933ac7e7358
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-25 13:00:34 -0300

    Create author from slack id

--- links/services/slack.py
+++ links/services/slack.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, BaseUserManager
 from slackclient import SlackClient
 from knowledge.settings import SLACK_TOKEN, SLACK_CHANNEL_ID, SLACK_BOT_NAME
+from .utils import send_created_user_email
 
 
 def send_notification_to_slack(link):
@@ -9,6 +10,15 @@ def send_notification_to_slack(link):
     sc = SlackClient(SLACK_TOKEN)
     sc.api_call('chat.postMessage', channel=SLACK_CHANNEL_ID, text=text_formatted,
         username=SLACK_BOT_NAME, icon_emoji=':information_desk_person:')
+
+
+def create_author(user):
+    password = BaseUserManager().make_random_password()
+    user.set_password(password)
+    user.save()
+    send_created_user_email(password)
+
+    return user
 
 
 def get_slack_user(slack_user_id):
@@ -20,4 +30,7 @@ def get_slack_user(slack_user_id):
 
     user, created = User.objects.get_or_create(email=email, is_superuser=True, username=username, is_staff=True)
 
-    return user, created
+    if created:
+        user = create_author(user)
+
+    return user
