9b6e1aff80b5477cad7d986b87e0f87ec024426f
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-25 12:48:23 -0300

    Get the information from the slack user to create the author

--- links/services/slack.py
+++ links/services/slack.py
@@ -9,3 +9,15 @@ def send_notification_to_slack(link):
     sc = SlackClient(SLACK_TOKEN)
     sc.api_call('chat.postMessage', channel=SLACK_CHANNEL_ID, text=text_formatted,
         username=SLACK_BOT_NAME, icon_emoji=':information_desk_person:')
+
+
+def get_slack_user(slack_user_id):
+    sc = SlackClient(SLACK_TOKEN)
+
+    profile = sc.api_call('users.profile.get', user=slack_user_id).get('profile')
+    email = profile.get('email')
+    username = profile.get('first_name').lower()
+
+    user, created = User.objects.get_or_create(email=email, is_superuser=True, username=username, is_staff=True)
+
+    return user, created
