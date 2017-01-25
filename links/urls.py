a5f63b59eb05612e62a10ea7e7e9da20efbc7df1
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-25 12:23:52 -0300

    Remove CreateSlackNewView from the links app

--- links/urls.py
+++ links/urls.py
@@ -5,5 +5,4 @@ from links import views
 urlpatterns = [
     url(r'^$', views.ListLinksView.as_view(), name="list-links"),
     url(r'^create/$', views.CreateLinkView.as_view(), name="create_new_link"),
-    url(r'^api/link/$', views.CreateSlackNewLinkView.as_view(), name="api-link"),
 ]
