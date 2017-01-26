cf90ab9715a3295978afd929c670c121eedfabfc
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-26 14:01:07 -0300

    Pass directly link.url instead of text_formatted

--- core/services/slack.py
+++ core/services/slack.py
@@ -3,8 +3,7 @@ from knowledge.settings import SLACK_TOKEN, SLACK_CHANNEL_ID, SLACK_BOT_NAME
 
 
 def send_notification_to_slack(link):
-    text_formatted = "{}".format(link.url)
 
     sc = SlackClient(SLACK_TOKEN)
-    sc.api_call('chat.postMessage', channel=SLACK_CHANNEL_ID, text=text_formatted,
+    sc.api_call('chat.postMessage', channel=SLACK_CHANNEL_ID, text=link.url,
         username=SLACK_BOT_NAME, icon_emoji=':information_desk_person:', unfurl_links=True)
