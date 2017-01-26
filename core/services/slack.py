62c926c4b4008f93125fa1db89222161158dab03
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-26 13:44:55 -0300

    Only show the Url of the link in Cintia's message

--- core/services/slack.py
+++ core/services/slack.py
@@ -3,7 +3,7 @@ from knowledge.settings import SLACK_TOKEN, SLACK_CHANNEL_ID, SLACK_BOT_NAME
 
 
 def send_notification_to_slack(link):
-    text_formatted = "{}\n{}".format(link.title, link.url)
+    text_formatted = "{}".format(link.url)
 
     sc = SlackClient(SLACK_TOKEN)
     sc.api_call('chat.postMessage', channel=SLACK_CHANNEL_ID, text=text_formatted,
