f9abe00f07a3452ea8a6c074f07cc5ace6da1337
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-27 14:31:22 -0300

    Add login and logout urls

--- links/urls.py
+++ links/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 
 from links import views
 
 urlpatterns = [
     url(r'^$', views.ListLinksView.as_view(), name="list-links"),
     url(r'^create/$', views.CreateLinkView.as_view(), name="create_new_link"),
+    url(r'^login/$', views.LoginView.as_view(), name="login"),
+    url(r'^accounts/logout/$', logout, {'next_page': 'links:list-links'}, name="logout"),
 ]
