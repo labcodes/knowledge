6176f0c875c5774b699cb6eb0b00d6c239dda4c5
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-27 14:19:50 -0300

    Add login to the knowledge web app

--- links/views.py
+++ links/views.py
@@ -20,3 +20,24 @@ class CreateLinkView(CreateView):
     template_name = 'links/create-link-form.html'
     success_url = reverse_lazy('links:list-links')
     form_class = LinkForm
+
+
+class LoginView(View):
+
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(username=user.username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect(reverse_lazy('links:list-links'))
+            else:
+                return render(request, 'links/index.html', {'login_error': 'Wrong email or password'})
+
+        except User.DoesNotExist:
+            return render(request, 'links/index.html', {'login_error': 'Wrong email or password'})
