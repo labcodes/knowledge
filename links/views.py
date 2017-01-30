b46681b721491b246c75955cf7c5e1d26fd6d23c
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-30 12:48:12 -0300

    Use login required to access the index page

--- links/views.py
+++ links/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 
@@ -9,7 +10,7 @@ from .models import Link
 from .forms import LinkForm
 
 
-class ListLinksView(ListView):
+class ListLinksView(LoginRequiredMixin, ListView):
     model = Link
     context_object_name = 'links'
     paginate_by = settings.LINKS_PER_PAGE
