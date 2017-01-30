7cfc099cb480b05355c4ce78a6944731a529dbcb
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-30 11:44:07 -0300

    Move login and logout urls from the links app to the core app

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.views import logout, login
 from django.views.generic import RedirectView
 
 from core import views
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url=reverse_lazy('links:list-links'))),
+    url(r'^accounts/login/$', login, {'template_name': 'core/login.html'}, name="login"),
+    url(r'^accounts/logout/$', logout, {'next_page': 'core:login'}, name="logout"),
 ]
