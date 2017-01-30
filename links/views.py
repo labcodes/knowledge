30e72ee8ac437558c6ca137ad90a6715a4f7eb78
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-30 11:47:45 -0300

    Remove LoginView

--- links/views.py
+++ links/views.py
@@ -1,11 +1,7 @@
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
-
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 
-from django.views.generic import CreateView, View
+from django.views.generic import CreateView
 
 from django.conf import settings
 
@@ -24,24 +20,3 @@ class CreateLinkView(CreateView):
     template_name = 'links/create-link-form.html'
     success_url = reverse_lazy('links:list-links')
     form_class = LinkForm
-
-
-class LoginView(View):
-
-    def post(self, request):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        try:
-            user = User.objects.get(email=email)
-            user = authenticate(username=user.username, password=password)
-
-            if user is not None:
-                login(request, user)
-
-                return redirect(reverse_lazy('links:list-links'))
-            else:
-                return render(request, 'links/index.html', {'login_error': 'Wrong email or password'})
-
-        except User.DoesNotExist:
-            return render(request, 'links/index.html', {'login_error': 'Wrong email or password'})
