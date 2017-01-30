76e39ce06c73b3353294e848d1281a5bd8d5466a
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-30 12:17:37 -0300

    Change the email message to use your email instead of first name

--- core/services/utils.py
+++ core/services/utils.py
@@ -2,6 +2,6 @@ from django.core.mail import EmailMessage
 
 
 def send_created_user_email(password, email):
-    message = 'You can login the Knowledge admin with your username: \'Your first name\' and password: ' + password
+    message = 'You can login the Knowledge admin with your username: \'Your email\' and password: ' + password
     email = EmailMessage('Knowledge Email', message, to=[email])
     email.send()
