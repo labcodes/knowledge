6645d5537dd817c33d64f66689b9ac7ba7d10e4e
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-31 10:24:48 -0300

    If the link has not http in it, add to the beginning of the string for the request work

--- links/utils.py
+++ links/utils.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_title_from_url(slack_url):
+    if 'http' not in slack_url:
+        slack_url = 'http://' + slack_url
+
     soup_url = BeautifulSoup(requests.get(slack_url).text)
     try:
         title = soup_url.find('meta', property='og:title').get('content')
