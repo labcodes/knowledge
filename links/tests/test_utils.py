6007b5bd703742541c3ea179c5afa2b798b7ced7
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-31 10:51:26 -0300

    Add tests for the check_for_http_in_url function

--- links/tests/test_utils.py
+++ links/tests/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from links.utils import get_title_from_url
+from links.utils import get_title_from_url, check_for_http_in_url
 
 
 @pytest.mark.django_db
@@ -10,3 +10,13 @@ def test_get_title_from_url_with_meta_title(client):
 @pytest.mark.django_db
 def test_get_title_from_url_without_meta_title(client):
     assert get_title_from_url('https://teamtreehouse.com/home') == 'Treehouse | Sign In'
+
+
+@pytest.mark.django_db
+def test_check_for_http_in_url_with_http_in_url(client):
+    assert check_for_http_in_url('https://api.slack.com/') == 'https://api.slack.com/'
+
+
+@pytest.mark.django_db
+def test_check_for_http_in_url_without_http_in_url(client):
+    assert check_for_http_in_url('api.slack.com/') == 'http://api.slack.com/'
