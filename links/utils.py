0f12000f3c501e3a22fbcde714183a597d484a43
Alessandro <alessandro.henrique@labcodes.com.br>
2017-01-31 10:42:43 -0300

    Add check_for_http_in_url helper function to add 'http://' if needed

--- links/utils.py
+++ links/utils.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def get_title_from_url(slack_url):
-    if 'http' not in slack_url:
-        slack_url = 'http://' + slack_url
+    slack_url = check_for_http_in_url(slack_url)
 
     soup_url = BeautifulSoup(requests.get(slack_url).text)
     try:
@@ -13,3 +12,7 @@ def get_title_from_url(slack_url):
         title = soup_url.find('title').string
 
     return title
+
+
+def check_for_http_in_url(slack_url):
+    return 'http://' + slack_url if 'http' not in slack_url else slack_url
