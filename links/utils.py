76faa4f7856750aa85fd4b9d1462035f14f72537
Alessandro <alessandro.henrique@labcodes.com.br>
2017-02-03 15:11:25 -0300

    Check if the link can be reached to ensure that the link exists

--- links/utils.py
+++ links/utils.py
@@ -5,9 +5,19 @@ from bs4 import BeautifulSoup
 def get_title_from_url(slack_url):
     slack_url = ensure_http_prefix(slack_url)
 
-    soup_url = BeautifulSoup(requests.get(slack_url).text)
     try:
-        title = soup_url.find('meta', property='og:title').get('content')
+        soup_url = BeautifulSoup(requests.get(slack_url).text)
+    except:
+        raise ConnectionError
+
+    try:
+        title = soup_url.find('meta', property='og:title')
+
+        if title:
+            title = soup_url.get('content')
+        else:
+            title = soup_url.find('title').string
+
     except AttributeError:
         title = soup_url.find('title').string
 
