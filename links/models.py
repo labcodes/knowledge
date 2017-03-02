226426f5dbe9ad71fa9f8863fa084f79478a8b0b
Alessandro <alessandro.henrique@labcodes.com.br>
2017-03-02 12:55:18 -0300

    Add the tags argument when creating a new Link

--- links/models.py
+++ links/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from core.services.slack import send_notification_to_slack, get_slack_user
-from links.utils import get_title_from_url, ensure_http_prefix
+from links.utils import get_title_from_url, ensure_http_prefix, get_tags_from_text
 
 from tagging.models import TagManager
 from tagging.fields import TagField
@@ -10,12 +10,13 @@ from tagging.fields import TagField
 
 class LinkManager(models.Manager):
 
-    def create_from_slack(self, slack_url, slack_user_id):
+    def create_from_slack(self, text, slack_url, slack_user_id):
+        tags = get_tags_from_text(text)
         title = get_title_from_url(slack_url)
         author = get_slack_user(slack_user_id)
         slack_url = ensure_http_prefix(slack_url)
 
-        return self.create(title=title, url=slack_url, author=author)
+        return self.create(title=title, url=slack_url, author=author, tags=tags)
 
 
 class Link(models.Model):
