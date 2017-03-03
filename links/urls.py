31e710dade37a5eb0406c41bb2c410a21de65b26
Alessandro <alessandro.henrique@labcodes.com.br>
2017-03-03 12:31:50 -0300

    Add optional parameter to the ListLinksView to filter links by tag

--- links/urls.py
+++ links/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from links import views
 
 urlpatterns = [
-    url(r'^$', views.ListLinksView.as_view(), name="list-links"),
+    url(r'^(?P<tag>\w+)?$', views.ListLinksView.as_view(), name="list-links"),
     url(r'^create/$', views.CreateLinkView.as_view(), name="create_new_link"),
 ]
