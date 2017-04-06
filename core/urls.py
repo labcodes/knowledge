b25f16cbf2f11c3f88f2394cd625c8144b35e8f1
Alessandro <alessandro.henrique@labcodes.com.br>
2017-04-06 13:31:51 -0300

    Remove login and logout django auth views

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,4 @@ from core import views
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url=reverse_lazy('links:list-links'))),
-    url(r'^accounts/login/$', login, {'template_name': 'core/login.html'}, name="login"),
-    url(r'^accounts/logout/$', logout, {'next_page': 'core:login'}, name="logout"),
 ]
