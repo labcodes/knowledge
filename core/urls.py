ae0a7117de0444bafbc9be8403bd7a97d789e54f
Fernando Lins <fernando@labcodes.com.br>
2017-05-02 17:43:59 -0300

    Remove imports unused.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
-from django.contrib.auth.views import logout, login
 from django.views.generic import RedirectView
 
-from core import views
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url=reverse_lazy('api:djoser:login'))),
